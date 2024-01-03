56389bf66ce47e86dd416110ad486a4abfb6cee8
Shailesh yadav <69782147+shaileshyadv1993@users.noreply.github.com>
2024-01-03 16:35:57 +0000

    datatype conversion-1

---
+++ 01_basics/03_comversionoOperators.js
@@ -0,0 +1,21 @@
+let score =55;
+console.log(typeof score);
+
+let myNumber = "56"
+let converedDataType = Number(myNumber)
+console.log(converedDataType);
+
+
+// "33"  => 33
+// "33avf" => Nan Number type
+
+// true => 1; false => 0
+
+let isLoggedIn = 6;
+let boleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(boleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "shailesh" => true
+
