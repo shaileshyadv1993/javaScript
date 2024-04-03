6f9ff3025efb03e963a76735763c4622fc7f20d7
Shailesh yadav <69782147+shaileshyadv1993@users.noreply.github.com>
2024-04-03 00:15:56 +0000

    datatypes-summary

---
+++ datatypes-summary.js
@@ -0,0 +1,14 @@
+// #primitive datatype
+// get copy of memory vale
+// 7 types : String  (call by value), Number, Boolean, null, undefined, Symbol, BigInt
+
+
+const id = Symbol("shailesh") // Unique
+const otherId = Symbol("shailesh")
+
+console.log(id === otherId)
+
+// Reference type (non-primitive)
+// get direct memory address
+// Array, Ojects, Functions
+
