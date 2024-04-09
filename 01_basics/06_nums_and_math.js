f81c26609702d69bfeffa435a2a6776f8d3df4e3
Shailesh yadav <69782147+shaileshyadv1993@users.noreply.github.com>
2024-04-09 02:09:12 +0000

    NUms and maths

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,22 @@
+const score = 100;
+// console.log(score)
+
+const newNumber = new Number(500)
+// console.log(newNumber)
+
+const myValue = 57687896543
+// console.log(myValue.toLocaleString('en-IN'))
+// console.log()
+
+// --------------- Maths --------------
+// console.log(Math)
+// console.log(Math.abs(-45))
+// console.log(Math.round(5.675))
+// console.log(Math.ceil(5.675)) // takes upper number
+// console.log(Math.floor(5.675)) // takes lower number
+// console.log(Math.min(4,6,7,3,1.6))
+console.log(Math.floor(Math.random()*10)+1)
+const min = 1
+const max = 6
+
+console.log((Math.floor(Math.random()*(max-min+1)))+min)
